858346af2940007922af1ae238fb1d826545b80f
Paul Sheehan <sheehanp@hotmail.com>
2019-06-03 20:59:03 +0100

    Get working but Post not working; cors corrected

--- tasks.js
+++ tasks.js
@@ -3,10 +3,10 @@ const serverless = require("serverless-http");
 const app = express();
 const mysql = require('mysql');
 
-/*
+
 const cors = require('cors');
 app.use(cors());
-*/
+
 
 app.use(express.json());
 
