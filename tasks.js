a42b6387ceeccf335588c9fa6cf4bcb687cc1a10
Paul Sheehan <sheehanp@hotmail.com>
2019-06-03 17:50:53 +0100

    updated as per week 11 lecture video to query the AWS database

--- tasks.js
+++ tasks.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "todoapplication"
+});
 
+app.get("/tasks", function(request, response) {
+  const username = request.query.username;
 
-app.get("/tasks", function (request, response) {
-
-  const tasklist = [
-    {taskid: 1, completed: true,  taskname: 'Get up'},
-    {taskid: 2, completed: false, taskname: 'Get out of bed'},
-    {taskid: 3, completed: true,  taskname: 'Drag a comb across my head'}
-  ];
-
-  response.json(tasklist);
+  let queryToExecute = "SELECT * FROM Task";
   
+  if (username) {
+    query =
+      "SELECT * FROM Task JOIN User on Task.UserId = User.UserId " +
+      "WHERE User.Username = " + connection.escape(username);
+  }
+
+  connection.query(queryToExecute, (err, queryResults) => {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      response.status(500).json({
+        error: err
+      });
+      
+    } else {
+      response.json({
+        tasks: queryResults
+      });
+    }
+  });
 });
 
 module.exports.handler = serverless(app);
